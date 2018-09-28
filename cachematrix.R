336cc2919b42fe27f5a471b83a857c49b837a58b
Edmundo Carmona Antoranz <eantoranz@gmail.com>
2018-09-28 14:10:01 -0600

    Assignment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invertValue <- NULL
+        
+        setMatrix <- function(y) {
+            x <<- y
+            invertValue <<- NULL
+        }
+        getMatrix <- function() x
+        setInvert <- function(invert) invertValue <<- invert
+        getInvert <- function() invertValue
+        list(setMatrix = setMatrix, getMatrix = getMatrix, setInvert = setInvert, getInvert = getInvert)
 }
 
 
@@ -12,4 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        cache <- x$getInvert()
+        if (!is.null(cache)) {
+            message("Getting value from cache")
+            return(cache)
+        }
+        # cache was not set so we need to save it
+        invert <- solve(x$getMatrix(), ...)
+        x$setInvert(invert)
+        # return invert
+        invert
 }
